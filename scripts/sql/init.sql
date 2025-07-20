6839c56b9910a5a794a3d34c24cc2f2ced505df8
Arunpsrj <arunqualityengineer@gmail.com>
2025-07-20 13:20:17 +0530

    Create init.sql

---
+++ scripts/sql/init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE CTMQA.dbo.profile_temp (
+	TempId varchar(10) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	FirstName varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	LastName varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	CONSTRAINT PK__profile___06C703C1C7459CF3 PRIMARY KEY (TempId)
+);
